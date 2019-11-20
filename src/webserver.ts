f613cbeb08f1ecad4d6bf61380f0d6e7bcb76fd0
kkm <kubasurdej@gmail.com>
2019-11-20 17:20:40 +0100

    Allow setting different mimeType

--- src/webserver.ts
+++ src/webserver.ts
@@ -1,9 +1,9 @@
 import { createServer } from 'http'
 
-export const serveContent = (content: string) => {
+export const serveContent = (content: string, mimeType: string = 'text/html') => {
   createServer((_reqest, response) => {
     response.setHeader('Content-Length', content.length)
-    response.setHeader('Content-Type', 'text/html')
+    response.setHeader('Content-Type', mimeType)
     response.statusCode = 200
     response.end(content)
   })
