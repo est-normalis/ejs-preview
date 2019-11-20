d399a0236107dbff0dc1f9558ee915c40366d374
kkm <kubasurdej@gmail.com>
2019-11-20 20:13:55 +0100

    Throw an error if file does not exist

--- src/render.ts
+++ src/render.ts
@@ -1,9 +1,17 @@
 import { render as renderEJS } from 'ejs'
-import { readFileSync } from 'fs'
+import { existsSync, readFileSync } from 'fs'
 
 const render = (templatePath: string, variables: any = {}) => {
+  if (!checkIfFileExists(templatePath)) {
+    throw new Error('File does not exist')
+  }
+
   const template = readFileSync(templatePath, 'utf-8')
   return renderEJS(template, variables)
+}
+
+const checkIfFileExists = (filePath: string): boolean => {
+  return !!existsSync(filePath)
 }
 
 export default render
