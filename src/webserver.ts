19bc82978e69b4f89bee3eb7fd52263979d0be33
kkm <kubasurdej@gmail.com>
2019-11-20 17:19:57 +0100

    Change serveFile function to serveContent

--- src/webserver.ts
+++ src/webserver.ts
@@ -1,13 +1,10 @@
-import { readFile } from 'fs'
 import { createServer } from 'http'
 
-export const serveFile = (file: string) => {
-    createServer((_reqest, response) => {
-        readFile(file, (_error, content) => {
-            response.setHeader("Content-Length", content.length)
-            response.setHeader("Content-Type", "text/html")
-            response.statusCode = 200
-            response.end(content)
-        })
-    })
+export const serveContent = (content: string) => {
+  createServer((_reqest, response) => {
+    response.setHeader('Content-Length', content.length)
+    response.setHeader('Content-Type', 'text/html')
+    response.statusCode = 200
+    response.end(content)
+  })
 }
